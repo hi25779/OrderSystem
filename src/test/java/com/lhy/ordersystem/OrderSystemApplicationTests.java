c730c5409b18db159dda941b794f7d436bef5f52
hi25779 <859028027@qq.com>
2022-08-08 00:15:00 +0800

    init

---
+++ src/test/java/com/lhy/ordersystem/OrderSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lhy.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+
+}
