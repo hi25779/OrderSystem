52038fb37873d5188a92535b16e5e254ff22e5d5
hi25779 <859028027@qq.com>
2022-08-14 23:26:34 +0800

    add mapper

---
+++ src/main/java/com/lhy/ordersystem/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.lhy.ordersystem.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lhy.ordersystem.entity.User;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : Jesse(lhy)
+ * @mail : 859028027@qq.com
+ * @created : 2022-08-14
+ **/
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ src/test/java/com/lhy/ordersystem/MapperTest.java
@@ -0,0 +1,27 @@
+package com.lhy.ordersystem;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.lhy.ordersystem.entity.User;
+import com.lhy.ordersystem.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * @author : Jesse(lhy)
+ * @mail : 859028027@qq.com
+ * @created : 2022-08-14
+ **/
+@SpringBootTest
+public class MapperTest {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void userMapperTest() {
+        List<User> list = userMapper.selectList(null);
+        System.out.println(list);
+    }
+}
